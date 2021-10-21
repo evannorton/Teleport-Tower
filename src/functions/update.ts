43d2c3935b01c0868b670cb9446b49ce06b103c4
Evan Norton <evan@retro-mmo.com>
2021-10-21 18:10:12 -0400

    abandon real time timekeeping

--- src/functions/update.ts
+++ src/functions/update.ts
@@ -3,9 +3,10 @@ import Player from "../classes/Player";
 import Projectile from "../classes/Projectile";
 import definables from "../maps/definables";
 import state from "../state";
+import ticksPerSecond from "../constants/ticksPerSecond";
 
 const update = (): void => {
-    state.now = performance.now();
+    state.now += 1000 / ticksPerSecond;
     const players: Map<string, Definable> | undefined = definables.get("Player");
     const projectiles: Map<string, Definable> | undefined = definables.get("Projectile");
     if (typeof players !== "undefined") {
