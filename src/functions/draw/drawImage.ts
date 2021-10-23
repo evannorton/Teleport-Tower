c6a84a15cf9e38d96778ac272e40d37dc7818170
Evan Norton <evan@retro-mmo.com>
2021-10-23 01:26:03 -0400

    no sprite cache

--- src/functions/draw/drawImage.ts
+++ src/functions/draw/drawImage.ts
@@ -6,36 +6,27 @@ import screenHeight from "../../constants/screenHeight";
 import screenWidth from "../../constants/screenWidth";
 import state from "../../state";
 
-const sprites: Map<string, Sprite> = new Map;
 const drawImage = (image: ImageSource, sourceX: number, sourceY: number, sourceWidth: number, sourceHeight: number, x: number, y: number, width: number, height: number, zIndex: number): void => {
     if (x + width > 0 && x < screenWidth && y + height > 0 && y < screenHeight && image.getIsLoaded() && state.app !== null) {
         const texture: BaseTexture | null = image.getBaseTexture();
         if (texture !== null) {
-            const key: string = `${image.getSlug()}-${sourceX}-${sourceY}-${sourceWidth}-${sourceHeight}-${x}-${y}-${width}-${height}-${zIndex}`;
-            const cached: Sprite | undefined = sprites.get(key);
-            if (typeof cached === "undefined") {
-                const chopX: number = Math.max(x * -1, 0);
-                const chopY: number = Math.max(y * -1, 0);
-                const adjustedX: number = Math.max(x, 0);
-                const adjustedY: number = Math.max(y, 0);
-                const adjustedWidth: number = Math.min(width - chopX, screenWidth - adjustedX);
-                const adjustedHeight: number = Math.min(height - chopY, screenHeight - adjustedY);
-                const adjustedSourceX: number = chopX + sourceX;
-                const adjustedSourceY: number = chopY + sourceY;
-                const adjustedSourceWidth: number = Math.min(sourceWidth - chopX, adjustedWidth);
-                const adjustedSourceHeight: number = Math.min(sourceHeight - chopY, adjustedHeight);
-                const sprite: Sprite = new Sprite(new Texture(texture, new Rectangle(adjustedSourceX, adjustedSourceY, adjustedSourceWidth, adjustedSourceHeight)));
-                sprite.x = Math.round(adjustedX);
-                sprite.y = Math.round(adjustedY);
-                sprite.width = adjustedWidth;
-                sprite.height = adjustedHeight;
-                sprite.zIndex = zIndex;
-                state.app.stage.addChild(sprite);
-                sprites.set(key, sprite);
-            }
-            else {
-                state.app.stage.addChild(cached);
-            }
+            const chopX: number = Math.max(x * -1, 0);
+            const chopY: number = Math.max(y * -1, 0);
+            const adjustedX: number = Math.max(x, 0);
+            const adjustedY: number = Math.max(y, 0);
+            const adjustedWidth: number = Math.min(width - chopX, screenWidth - adjustedX);
+            const adjustedHeight: number = Math.min(height - chopY, screenHeight - adjustedY);
+            const adjustedSourceX: number = chopX + sourceX;
+            const adjustedSourceY: number = chopY + sourceY;
+            const adjustedSourceWidth: number = Math.min(sourceWidth - chopX, adjustedWidth);
+            const adjustedSourceHeight: number = Math.min(sourceHeight - chopY, adjustedHeight);
+            const sprite: Sprite = new Sprite(new Texture(texture, new Rectangle(adjustedSourceX, adjustedSourceY, adjustedSourceWidth, adjustedSourceHeight)));
+            sprite.x = Math.round(adjustedX);
+            sprite.y = Math.round(adjustedY);
+            sprite.width = adjustedWidth;
+            sprite.height = adjustedHeight;
+            sprite.zIndex = zIndex;
+            state.app.stage.addChild(sprite);
         }
     }
 };
