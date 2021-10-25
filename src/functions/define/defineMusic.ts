95c9ece2ce9c47139da71543487005b7a0fcebdd
Evan Norton <evan@retro-mmo.com>
2021-10-25 07:26:20 -0400

    clouds music

--- src/functions/define/defineMusic.ts
+++ src/functions/define/defineMusic.ts
@@ -2,6 +2,7 @@ import Music from "../../classes/Music";
 
 const defineMusic = (): void => {
     new Music("inside", 3, 14.76, "part2", 87 * 16, 174 * 16);
+    new Music("clouds", 4, 55.9, "part2", 0 * 16, 87 * 16);
 };
 
 export default defineMusic;
