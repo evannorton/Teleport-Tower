36909c5d201dd82c5c16c45e76a24c7e088840e2
Evan Norton <evan@retro-mmo.com>
2021-10-25 03:13:26 -0400

    2nd section in part 2

--- src/functions/define/defineMusic.ts
+++ src/functions/define/defineMusic.ts
@@ -1,7 +1,7 @@
 import Music from "../../classes/Music";
 
 const defineMusic = (): void => {
-    new Music("inside", 3, 14.76, "part2", 0, 87 * 16);
+    new Music("inside", 3, 14.76, "part2", 44 * 16, 131 * 16);
 };
 
 export default defineMusic;
