eea48071778cc37e731ea693a424388c3baa4eb1
Evan Norton <evan@retro-mmo.com>
2021-10-25 00:03:45 -0400

    updated cutscene

--- src/functions/define/defineCutscenes.ts
+++ src/functions/define/defineCutscenes.ts
@@ -1,7 +1,7 @@
 import Cutscene from "../../classes/Cutscene";
 
 const defineCutscenes = (): void => {
-    new Cutscene("intro", 81, 8);
+    new Cutscene("intro", 93, 8);
 };
 
 export default defineCutscenes;
