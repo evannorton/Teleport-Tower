9ae2b338daa4963f3f075ef2fe4d0f2609cfe179
Evan Norton <evan@retro-mmo.com>
2021-10-25 14:45:40 -0400

    python

--- src/maps/tilesets.ts
+++ src/maps/tilesets.ts
@@ -1,11 +1,13 @@
 import TiledTileset from "../interfaces/tiled/TiledTileset";
 import clouds from "../tiled/tilesets/clouds.json";
+import python from "../tiled/tilesets/python.json";
 import street from "../tiled/tilesets/street.json";
 import transports from "../tiled/tilesets/transports.json";
 import wall from "../tiled/tilesets/wall.json";
 
 const tilesets: Map<string, TiledTileset> = new Map;
 tilesets.set("clouds", clouds);
+tilesets.set("python", python);
 tilesets.set("street", street);
 tilesets.set("transports", transports);
 tilesets.set("wall", wall);
